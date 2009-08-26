e5d87d6c94b7e189a541e3f5be8632accd74ac37
Adrian L Lange <adrianlund@gmail.com>
2009-08-27 00:23:03 +0200

    Removed tooltip

--- HatTrick.lua
+++ HatTrick.lua
@@ -1,12 +1,3 @@
-
-local GameTooltip = GameTooltip
-
-local L = {
-	helmtip = "Toggles helmet model.",
-	cloaktip = "Toggles cloak model.",
-}
-
-
 -- I hate that I have to do this, but Blizz's option frame fucks up the setting unless it was set from their option frame
 local ShowCloak, ShowHelm, noop = ShowCloak, ShowHelm, function() end
 _G.ShowCloak, _G.ShowHelm = noop, noop
@@ -16,22 +7,15 @@ for _,check in pairs{InterfaceOptionsDisplayPanelShowCloak, InterfaceOptionsDisp
 	check.Enable = noop
 end
 
-
 local hcheck = CreateFrame("CheckButton", "HelmCheckBox", PaperDollFrame, "OptionsCheckButtonTemplate")
 hcheck:ClearAllPoints()
 hcheck:SetWidth(22)
 hcheck:SetHeight(22)
 hcheck:SetPoint("TOPLEFT", CharacterHeadSlot, "BOTTOMRIGHT", 5, 5)
 hcheck:SetScript("OnClick", function() ShowHelm(not ShowingHelm()) end)
-hcheck:SetScript("OnEnter", function()
- 	GameTooltip:SetOwner(this, "ANCHOR_RIGHT")
-	GameTooltip:SetText(L.helmtip)
-end)
-hcheck:SetScript("OnLeave", function() GameTooltip:Hide() end)
 hcheck:SetScript("OnEvent", function() hcheck:SetChecked(ShowingHelm()) end)
 hcheck:RegisterEvent("UNIT_MODEL_CHANGED")
 hcheck:SetToplevel(true)
-
 
 local ccheck = CreateFrame("CheckButton", "CloakCheckBox", PaperDollFrame, "OptionsCheckButtonTemplate")
 ccheck:ClearAllPoints()
@@ -39,11 +23,6 @@ ccheck:SetWidth(22)
 ccheck:SetHeight(22)
 ccheck:SetPoint("TOPLEFT", CharacterBackSlot, "BOTTOMRIGHT", 5, 5)
 ccheck:SetScript("OnClick", function() ShowCloak(not ShowingCloak()) end)
-ccheck:SetScript("OnEnter", function()
-	GameTooltip:SetOwner(this, "ANCHOR_RIGHT")
-	GameTooltip:SetText(L.cloaktip)
-end)
-ccheck:SetScript("OnLeave", function() GameTooltip:Hide() end)
 ccheck:SetScript("OnEvent", function() ccheck:SetChecked(ShowingCloak()) end)
 ccheck:RegisterEvent("UNIT_MODEL_CHANGED")
 ccheck:SetToplevel(true)
